5352f1db12c2067a426e294d63e02a54a5fe8d4a
cris93g <cristhianswork@gmail.com>
2020-08-03 15:43:20 -0500

    intial commit

---
+++ src/main/java/com/example/All4PetsandPets4All/All4PetsandPets4AllApplication.java
@@ -0,0 +1,13 @@
+package com.example.All4PetsandPets4All;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class All4PetsandPets4AllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(All4PetsandPets4AllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/All4PetsandPets4All/All4PetsandPets4AllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.All4PetsandPets4All;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class All4PetsandPets4AllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
