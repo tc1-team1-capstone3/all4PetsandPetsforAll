4c9191a542d75d420fc24fabd566d4ed78bf5d31
Sarah Bates <skbates00@me.com>
2020-08-05 14:58:28 -0500

    testing out css with cody and chiristian

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     <>
       <h1>Hello World</h1>
       <Link to='/admin'>
-        <h1>Admin</h1>
+        <h2>Admin</h2>
       </Link>
         <ItemDisplay productList={productList} admin={false} />
     </>
